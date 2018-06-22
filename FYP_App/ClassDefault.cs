f214369aac78667d01763b6a6589b8049ac71dee
RNImansyah <rn_imansyah@hotmail.com>
2018-06-22 18:37:14 +0800

    added register page

---
+++ FYP_App/ClassDefault.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace FYP_App
+{
+    public class ClassDefault
+    {
+        public static SqlConnection getConnection()
+        {
+            SqlConnection conn = null;
+
+            conn = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=C:\USERS\IMANSYAH\SOURCE\REPOS\FYP_APP\FYP_APP\APP_DATA\DB_1.MDF;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            return conn;
+        }
+
+        public static void executeQuery(String strQuery)
+        {
+
+
+            SqlConnection conn = getConnection();
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(strQuery, conn);
+            cmd.ExecuteNonQuery();
+            closeConnection(conn);
+
+        }
+        public static void closeConnection(SqlConnection conn)
+        {
+
+            if (conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+
+        }
+    }
+}
---
+++ FYP_App/RegisterPage.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FYP_App
+{
+    public partial class RegisterPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.QueryString["register"] != null)
+            {
+                string username = Request.Form["username"];
+                string password = Request.Form["password"];
+                string fname = Request.Form["fname"];
+                string lname = Request.Form["lname"];
+                string email = Request.Form["email"];
+                string contact = Request.Form["contact"];
+                string query = "insert into user_table (USERNAME, PASSWORD, FIRST_NAME, LAST_NAME, EMAIL, CONTACT, USER_TYPE) values ('" + username + "','" + password + "','" + fname + "','" + lname + "','" + email + "','" + contact + "','user')";
+                ClassDefault.executeQuery(query);
+                Response.Redirect("LoginPage.aspx");
+            }
+        }
+    }
+}
